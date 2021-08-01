37015e3f708ea30bcff8e1708f30bac7eee73718
Egor Ermolaev <probegrofanston@mail.ru>
2021-08-01 21:29:28 +0300

    created Encryptor

--- src/js/Encryptor.js
+++ src/js/Encryptor.js
@@ -1,3 +1,71 @@
-class Encryptor {
+import {BmpParser} from './BmpParser';
 
+export class Encryptor {
+  #CHAR_BORDER = '∇'
+  #offset = 0
+  #STEP = 3
+  #view
+  #bmpParser
+
+  /**
+   * @param {ArrayBuffer} buffer
+   */
+  constructor(buffer) {
+    this.#view = new DataView(buffer)
+    this.#bmpParser = new BmpParser(this.#view)
+  }
+  /**
+   * @param {String} sValue
+   * @returns {String}
+   */
+  decode(sValue) {
+    console.log(sValue);
+    console.log(String.fromCharCode(parseInt(sValue, 2)));
+    return String.fromCharCode(parseInt(sValue, 2))
+  }
+
+  /**
+   * @param {String} sValue
+   * @returns {Array.<String>}
+   */
+  encode(sValue) {
+    return sValue.split('').map(char => char.charCodeAt(0).toString(2))
+  }
+
+  encrypt() {
+    this.#offset = this.#bmpParser.offsetBits
+    const aBinaryChars = this.encode(this.#CHAR_BORDER + 'Привет' + this.#CHAR_BORDER)
+    aBinaryChars.join().split('').forEach(char => {
+      if (this.#offset >= (this.#bmpParser.fileSize - this.#STEP)) {
+        alert('Файл кончился!')
+      }
+
+      if (char === ',') this.#view.setUint8(this.#offset, 0)
+
+      /** @todo: Исправить .setUint8() */
+      this.#view.setUint8(this.#offset, +this.#view.getUint8(this.#offset) + +char)
+      this.#offset++
+    })
+
+
+    return this.#view
+    // console.log(this.decode('10001000000111')); // ∇
+  }
+
+  decipher() {
+    let cycleInProgress = true
+    this.#offset = this.#bmpParser.offsetBits
+    let string = ''
+
+    if (this.decode(this.#view.getUint8(this.#bmpParser.offsetBits)) !== this.#CHAR_BORDER) cycleInProgress = false
+
+    while (cycleInProgress) {
+      const char = this.decode(this.#view.getUint8(this.#offset))
+      if (char === this.#CHAR_BORDER) cycleInProgress = false
+      this.#offset++
+      string += char
+    }
+
+    return string
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import {BmpParser} from './BmpParser'
+import {Encryptor} from './Encryptor';
 
 const input = document.getElementById('file-input')
 const button = document.getElementById('decipher-button')
@@ -8,12 +8,11 @@ button.onclick = () => {
 }
 
 input.onchange = async event => {
-  console.log(123);
   const buffer = await event.target.files[0].arrayBuffer()
   const link = document.createElement('a')
-  const parserBMP = new BmpParser(buffer)
+  const encryptor = new Encryptor(buffer)
   link.download = 'test.bmp'
   link.innerText = 'Download'
-  link.href = URL.createObjectURL(new Blob([parserBMP.encrypt()], {type: 'image/bmp'}))
+  link.href = URL.createObjectURL(new Blob([encryptor.encrypt()], {type: 'image/bmp'}))
   document.querySelector('.test').appendChild(link)
 }
